ec95a824ea83eca3f086594c11725f92ddb20ce0
Julio Aguilar <julio.aguilar9928@gmail.com>
2021-06-17 18:15:49 -0500

    Add layout for simulator

--- lib/pento_web/live/simulator_live.ex
+++ lib/pento_web/live/simulator_live.ex
@@ -2,14 +2,22 @@ defmodule PentoWeb.SimulatorLive do
   use Phoenix.LiveView
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, ms: 500)}
-  end
-
-  def handle_event("change_form", %{"title" => title}, socket) do
-    {:noreply, assign(socket, title: title)}
+    {:ok, assign(socket, ms: 500, has_finished: false)}
   end
 
   def handle_event("change_form", %{"ms" => ms}, socket) do
-    {:noreply, assign(socket, ms: ms)}
+    {:noreply, assign(socket, ms: ms, has_finished: false)}
+  end
+
+  def handle_event("init", _params, socket) do
+    {:noreply, assign(socket, has_finished: true)}
+  end
+
+  def handle_event("change_status", %{"has_finished" => has_finished}, socket) do
+    {:noreply, assign(socket, has_finished: has_finished)}
+  end
+
+  def has_finished_class(has_finished) do
+    if has_finished == true, do: "simulator-package-end"
   end
 end
